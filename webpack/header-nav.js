a2188c783a4b9bfe40a154ab6906354206a3ac4f
Max Murdoch <max@maxmurdo.ch>
2018-02-02 16:14:53 +0000

    Increase header size

--- webpack/header-nav.js
+++ webpack/header-nav.js
@@ -1,24 +1,28 @@
 const setupTabs = () => {
-  const tabGroups = document.querySelectorAll('[data-tab-group]');
+  const tabGroups = document.querySelectorAll('[data-tab-group]')
 
   tabGroups.forEach(function(tabGroup) {
-    const tabs = tabGroup.querySelectorAll('[data-tab-group] [data-tab-for]');
+    const tabs = tabGroup.querySelectorAll('[data-tab-group] [data-tab-for]')
 
     const deactivate = function(element) {
-      element.classList.add('o-60');
-      element.classList.remove('o-100');
+      element.classList.add('o-60')
+      element.classList.remove('o-100')
 
-      const tabFor = tabGroup.querySelector('[data-tab-content="' + element.dataset.tabFor + '"]');
-      tabFor.classList.remove('is-visible');
-    };
+      const tabFor = tabGroup.querySelector(
+        '[data-tab-content="' + element.dataset.tabFor + '"]'
+      )
+      tabFor.classList.remove('is-visible')
+    }
 
     const activate = function(element) {
-      element.classList.remove('o-60');
-      element.classList.add('o-100');
+      element.classList.remove('o-60')
+      element.classList.add('o-100')
 
-      const tabFor = tabGroup.querySelector('[data-tab-content="' + element.dataset.tabFor + '"]');
-      tabFor.classList.add('is-visible');
-    };
+      const tabFor = tabGroup.querySelector(
+        '[data-tab-content="' + element.dataset.tabFor + '"]'
+      )
+      tabFor.classList.add('is-visible')
+    }
 
     tabs.forEach(function(tab) {
       tab.addEventListener('click', function(event) {
@@ -26,32 +30,32 @@ const setupTabs = () => {
         // if they are, deactivate
         tabs.forEach(function(tab) {
           if (tab !== event.target) {
-            deactivate(tab);
+            deactivate(tab)
           }
-        });
+        })
 
         // check if self is active
         // if is, deactivate
         if (event.target.classList.contains('fw6')) {
-          deactivate(event.target);
-          return;
+          deactivate(event.target)
+          return
         }
 
-        activate(event.target);
-      });
-    });
+        activate(event.target)
+      })
+    })
 
     // activate the first tab by default
     tabs.forEach(function(tab, index) {
       if (index === 0) {
-        activate(tab);
+        activate(tab)
       } else {
-        deactivate(tab);
+        deactivate(tab)
       }
-    });
-  });
+    })
+  })
 
-  console.log('Tabs set up');
-};
+  console.log('Tabs set up')
+}
 
-export default setupTabs;
+export default setupTabs
